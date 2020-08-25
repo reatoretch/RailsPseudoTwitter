2da76735cd8514841e124a7ab49ae320d72df81a
reatoretch <reatoretch@gmail.com>
2020-08-25 13:58:52 +0900

    Adjusted the code to fit my priorities

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :tweets
+
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
   resources :users, only: [:index, :show]
+  resources :tweets
+
   root to: 'registrations#new' # [!]Temporary name
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
