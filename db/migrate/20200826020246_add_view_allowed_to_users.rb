64a7d657046cbe1336e7228ec04349672662bf22
reatoretch <reatoretch@gmail.com>
2020-08-26 11:07:09 +0900

    setting default values in the migration file

--- db/migrate/20200826020246_add_view_allowed_to_users.rb
+++ db/migrate/20200826020246_add_view_allowed_to_users.rb
@@ -1,5 +1,5 @@
 class AddViewAllowedToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :view_allowed, :boolean
+    add_column :users, :view_allowed, :boolean, default: true, null: true
   end
 end
