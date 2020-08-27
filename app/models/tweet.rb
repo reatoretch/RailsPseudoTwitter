996ad3fd9407e6880e0758d17a7ae9c12b9a93b6
reatoretch <reatoretch@gmail.com>
2020-08-27 14:35:36 +0900

    Implementing an image display function using active storage

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  has_one_attached :upload_file
   
   validates :user, presence: true
   validates :content, presence: true, length: { in: 1..140 }
