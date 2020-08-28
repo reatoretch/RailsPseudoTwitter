3cb298a15cb54fe1461e163439236c6c2dabad12
reatoretch <reatoretch@gmail.com>
2020-08-28 18:10:57 +0900

    add vulnerability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   before_action :allow_iframe_requests
+  before_action :allow_origin
 
   def allow_iframe_requests
     response.headers.delete('X-Frame-Options')
+  end
+
+  def allow_origin
+    response.headers['Access-Control-Allow-Origin'] = "http://localhost:8000"
   end
 end
